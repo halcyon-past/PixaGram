0bc3666de0e99b0c0244a074911bb996c5fa3073
halcyon-past <titanssuperior@gmail.com>
2024-04-29 20:06:50 +0530

    Added some changes

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -16,9 +16,9 @@ export default function AuthLayout() {
           </section>
 
           <img
-            src="/assets/images/side-img.svg"
+            src="/assets/images/planet.jpeg"
             alt="logo"
-            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat rotate-180"
           />
         </>
       )}
